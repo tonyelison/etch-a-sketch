975bbffef7ed6feb098c32c62b1935e012da2eeb
Anthony Elison <tonyelison37@gmail.com>
2023-02-17 16:29:46 -0700

    Fill boxes on hover, add clear grid button

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ function drawGrid(sideLength) {
   const area = sideLength * sideLength;
   for (let i = 0; i < area; i++) {
     const box = document.createElement("div");
+    box.addEventListener("mouseover", fillBox);
     box.classList.add("box");
     grid.appendChild(box);
   }
 }
 
 drawGrid(16);
+
+function fillBox() {
+  this.classList.add("filled");
+}
+
+const clearButton = document.querySelector("button");
+clearButton.addEventListener("click", clearGrid);
+
+function clearGrid() {
+  grid.childNodes.forEach((box) => box.classList.remove("filled"));
+}
